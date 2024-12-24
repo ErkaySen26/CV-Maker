50f5b5b6f91a879dd4c1d68f91472de66a123c7d
ErkaySen26 <100082245+ErkaySen26@users.noreply.github.com>
2024-12-24 12:49:20 +0300

    app.jsx error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import './app.css';
+import './App.css';
 import Home from './components/home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
